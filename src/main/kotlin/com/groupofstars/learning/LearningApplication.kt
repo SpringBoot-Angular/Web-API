e1634b4db6975b9a797616cabeaa9a579c970265
groupofstars <groupofstars36145@gmail.com>
2024-08-22 00:15:40 +0900

    Create Spring Boot

---
+++ src/main/kotlin/com/groupofstars/learning/LearningApplication.kt
@@ -0,0 +1,15 @@
+package com.groupofstars.learning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningApplication{
+
+}
+@RestController
+class MessageControl
+
+fun main(args: Array<String>) {
+	runApplication<LearningApplication>(*args)
+}
